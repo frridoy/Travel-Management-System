c5df1d79ddfc1105a2738586aca89d4928afea68
Hridoy <foyzur.r.h@gmail.com>
2024-04-04 01:05:07 +0600

    Solve the problem Admin Sidebar

--- resources/views/Admin/Layouts/sidebar.blade.php
+++ resources/views/Admin/Layouts/sidebar.blade.php
@@ -9,9 +9,9 @@
                 <span class="sidebar-link-title">Package </span>
             </a>
             <ul class="sidebar-menu list-unstyled collapse" id="packageDropdown">
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted create" href="{{route('package.create')}}">Package Create</a></li>
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('package.list')}}">Package List</a></li>
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('package.trash')}}">Package Trash List</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted create" href="{{route('package.create')}}">Package- Create</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('package.list')}}">Package- List</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('package.trash')}}">Package- Trash List</a></li>
             </ul>
         </li>
 
@@ -23,9 +23,9 @@
                 <span class="sidebar-link-title">Hotel </span>
             </a>
             <ul class="sidebar-menu list-unstyled collapse" id="hotelDropdown">
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted create" href="{{route('hotel.create')}}">Hotel Create</a></li>
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('hotel.list')}}">Hotel List</a></li>
-                <li class="sidebar-list-item"><a class="sidebar-link text-muted warning" href="{{route('hotel.trash')}}">Hotel Trash List</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted create" href="{{route('hotel.create')}}">Hotel- Create</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('hotel.list')}}">Hotel- List</a></li>
+                <li class="sidebar-list-item"><a class="sidebar-link text-muted warning" href="{{route('hotel.trash')}}">Hotel- Trash List</a></li>
             </ul>
         </li>
 
@@ -44,13 +44,13 @@
         </li>
 
         <li class="sidebar-list-item">
-            <a class="sidebar-link text-muted" href="#" data-bs-target="#packageDropdown" role="button" aria-expanded="false" data-bs-toggle="collapse">
+            <a class="sidebar-link text-muted" href="#" data-bs-target="#destinationDropdown" role="button" aria-expanded="false" data-bs-toggle="collapse">
                 <svg class="svg-icon svg-icon-md me-3">
                     <use xlink:href="icons/orion-svg-sprite.71e9f5f2.svg#design-1"> </use>
                 </svg>
                 <span class="sidebar-link-title">Destination </span>
             </a>
-            <ul class="sidebar-menu list-unstyled collapse" id="packageDropdown">
+            <ul class="sidebar-menu list-unstyled collapse" id="destinationDropdown">
                 <li class="sidebar-list-item"><a class="sidebar-link text-muted create" href="{{route('destination.create')}}">Destination- Create</a></li>
                 <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="{{route('destination.list')}}">Destination- List</a></li>
                 <li class="sidebar-list-item"><a class="sidebar-link text-muted list" href="">Destination- Trash List</a></li>
@@ -60,3 +60,4 @@
     </ul>
 
 </div>
+
